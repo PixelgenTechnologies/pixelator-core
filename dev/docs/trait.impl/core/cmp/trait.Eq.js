03dfb866a000a15db2b4b6dec22345c8c1bd5870
Aratz <Aratz@users.noreply.github.com>
2026-04-14 11:31:04 +0000

    Deploying to gh-page from @ PixelgenTechnologies/pixelator-core@dbf189d119219921e99ec0c10bfc576f559865f7 🚀

--- dev/docs/trait.impl/core/cmp/trait.Eq.js
+++ dev/docs/trait.impl/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["pixelator_core",[["impl&lt;T: <a class=\"trait\" href=\"pixelator_core/common/types/trait.EdgeWeight.html\" title=\"trait pixelator_core::common::types::EdgeWeight\">EdgeWeight</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"pixelator_core/common/types/struct.Edge.html\" title=\"struct pixelator_core::common::types::Edge\">Edge</a>&lt;T&gt;",0]]]]);
+    const implementors = Object.fromEntries([["pixelator_core",[["impl&lt;T: <a class=\"trait\" href=\"pixelator_core/common/types/trait.EdgeWeight.html\" title=\"trait pixelator_core::common::types::EdgeWeight\">EdgeWeight</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"pixelator_core/common/types/struct.Edge.html\" title=\"struct pixelator_core::common::types::Edge\">Edge</a>&lt;T&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- dev/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ dev/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["pixelator_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"pixelator_core/common/io/struct.ParquetUMIPairIter.html\" title=\"struct pixelator_core::common::io::ParquetUMIPairIter\">ParquetUMIPairIter</a>",0]]]]);
+    const implementors = Object.fromEntries([["pixelator_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"pixelator_core/common/io/struct.ParquetUMIPairIter.html\" title=\"struct pixelator_core::common::io::ParquetUMIPairIter\">ParquetUMIPairIter</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
